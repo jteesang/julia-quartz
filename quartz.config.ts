2dda87be13eac0ecbdadd1c88a4662062e2ccffb
Julia Tseng <juliatseng12345@gmail.com>
2024-04-20 19:36:56 -0400

    Quartz sync: Apr 20, 2024, 7:36 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "jteesang.me",
+    baseUrl: "julia-quartz.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
